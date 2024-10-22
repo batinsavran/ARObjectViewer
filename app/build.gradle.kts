f68aef15263f59084e877bf8481c831645e0ac92
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-10-23 00:06:16 +0300

    feat: Integrate ARCore and Sceneform
    This commit integrates ARCore and Sceneform into the project, updates dependencies, and enables view binding.
    
    - Added ARCore and Sceneform dependencies.
    - Updated various dependencies to newer versions, including AppCompat, Material, and core-ktx.
    - Enabled view binding for UI interactions.
    - Updated the settings to include JitPack repository for Sceneform.
    - Enabled Jetifier for AndroidX compatibility.
    - Added a basic "Hello World" activity.
    - Configured build dependencies and settings.
    - Added gitignore files to exclude unnecessary files from version control.
    - Included default launcher icons and theme resources.
    - Added basic unit and instrumentation tests.
    - Enabled edge-to-edge display for immersive user experience.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,13 +41,15 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat.v161)
+    implementation(libs.material.v190)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.ar.core)
+    implementation(libs.sceneform.core)
+    implementation(libs.sceneform.ux)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
 }
